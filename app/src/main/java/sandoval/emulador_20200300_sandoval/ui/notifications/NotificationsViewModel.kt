695ba8a53d33c936b46f112dec7cd9c27a4ac87e
sandoval <20200300@ricaldone.edu.sv>
2024-04-27 19:11:20 -0600

    subi mi emulador

---
+++ app/src/main/java/sandoval/emulador_20200300_sandoval/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package sandoval.emulador_20200300_sandoval.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
