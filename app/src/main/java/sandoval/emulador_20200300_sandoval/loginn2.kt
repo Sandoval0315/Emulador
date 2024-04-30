ce6efa60d9ef52ef779bb1ad1bc1913e18cc18b6
sandoval <20200300@ricaldone.edu.sv>
2024-04-29 21:58:59 -0600

    termine la practica

---
+++ app/src/main/java/sandoval/emulador_20200300_sandoval/loginn2.kt
@@ -0,0 +1,28 @@
+package sandoval.emulador_20200300_sandoval
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class loginn2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_loginn2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val acceder = findViewById<Button>(R.id.btnacceder)
+        acceder.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
